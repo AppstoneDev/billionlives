c53919d29f0d52199921a1c04be0a6c61cda11b4
gautamakash91 <gautamakash91@gmail.com>
2022-05-23 14:01:37 +0530

    new project created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+var a = 10;
+var b = 100;
+let c = {
+  backgroundColor: "green",
+  width: 300,
+  height: 300
+};
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component{
+
+  showContent = () => {
+    a = 20;
+    b = 500;
+    console.log(a);
+    console.log(b);
+  }
+
+  render(){
+    b = 200;
+    return(
+      <div 
+        style={{
+          backgroundColor: "yellow",
+          width: 300,
+          height: 300
+        }}
+      >
+        Hello world
+        <button onClick={this.showContent}>
+          click me 
+        </button>
+        {a}
+        {b}
+      </div>
+    )
+  }
 }
-
-export default App;
